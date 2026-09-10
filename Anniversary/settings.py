6dfc27bb2b31472d6f72c3753278e27a3168729f
J0hnK3nn3th <salgadojohnkenneth98@gmail.com>
2026-09-10 19:12:01 +0800

    Initial Commit

--- Anniversary/settings.py
+++ Anniversary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&22l9tz=r+2)62$njcrn(c32-4ykf^wmq8lxvo5@2%!rc$3)k7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
